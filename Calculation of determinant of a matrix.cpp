4ab322d22b4ad6085ea06b015d37adf891d0e175
Dilpreet <92905896+dfordp@users.noreply.github.com>
2022-03-18 19:33:28 +0530

    Update Calculation of determinant of a matrix.cpp

--- Calculation of determinant of a matrix.cpp
+++ Calculation of determinant of a matrix.cpp
@@ -18,7 +18,7 @@ int determinant(int matrix[10][10], int n)
 		for (int x = 0; x < n; x++) 
 		{
 			int subi = 0;
-			for (int i = 1; i < n; i++)
+			for (int i = 0; i < n; i++)
 			{
 				int subj = 0;
 				for (int j = 0; j < n; j++)
